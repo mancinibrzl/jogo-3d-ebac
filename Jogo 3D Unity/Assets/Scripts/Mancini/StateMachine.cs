4b748387708e72813554e77266c21b7dfbad23e8
mancinibrzl <mariomancinineto2017@gmail.com>
2024-09-12 15:08:33 -0300

    Update StateMachine.cs

--- Jogo 3D Unity/Assets/Scripts/Mancini/StateMachine.cs
+++ Jogo 3D Unity/Assets/Scripts/Mancini/StateMachine.cs
@@ -2,50 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using System;
 
-public class StateMachine<T> where T : System.Enum
+namespace Ebac.StateMachine
 {
-    //chave
-    public Dictionary<T, StateBase> dictionaryState;
 
-    private StateBase _currentState;
-    public float timeToStartGame = 1f;
-
-    protected void RegisterStates(T typeEnum, StateBase state)
+    public class StateMachine<T> where T : System.Enum
     {
-        //dictionaryState = new Dictionary<T, StateBase>();
-        dictionaryState.Add(typeEnum, state);
+        public Dictionary<T, StateBase> dictionaryState;
 
-        //SwitchState(States.NONE);
+        private StateBase _currentState;
+        public float timeToStartGame = 1f;
 
-        //Invoke(nameof(StartGame), timeToStartGame);
-    }
-
-    [Button]
-    private void StartGame()
-    {
-        //SwitchState(States.NONE);
-    }
-
-    [Button]
-    private void SwitchState(T state)
-    {
-        if (_currentState != null) _currentState.OnStateExit();
-        
-        _currentState = dictionaryState[state];
-
-        _currentState.OnStateEnter();
-    }
-
-    private void Update()
-    {
-        if (_currentState != null) _currentState.OnStateStay();
-
-        if (Input.GetKeyDown(KeyCode.O))
+        public StateBase currentState
         {
-            //SwitchState(States.DEAD);
+            get {return _currentState;}
         }
+
+        public void Init()
+        {
+            dictionaryState = new Dictionary<T, StateBase>();
+        }
+
+        public void RegisterStates(T typeEnum, StateBase state)
+        {
+            dictionaryState.Add(typeEnum, state);
+        }
+        public void SwitchState(T state)
+        {
+            if (_currentState != null) _currentState.OnStateExit();
+        
+            _currentState = dictionaryState[state];
+
+            _currentState.OnStateEnter();
+        }
+
+        public void Update()
+        {
+            if (_currentState != null) _currentState.OnStateStay();
+        }
+
+
     }
-
-
 }
