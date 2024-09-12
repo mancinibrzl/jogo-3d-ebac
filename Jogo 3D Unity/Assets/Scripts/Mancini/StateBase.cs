c3a2adb29f6c96b5c65845928c3ba9ae58bb6b69
mancinibrzl <mariomancinineto2017@gmail.com>
2024-09-12 15:08:31 -0300

    Update StateBase.cs

--- Jogo 3D Unity/Assets/Scripts/Mancini/StateBase.cs
+++ Jogo 3D Unity/Assets/Scripts/Mancini/StateBase.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateBase
+namespace Ebac.StateMachine
 {
-    public virtual void OnStateEnter(object o = null)
+
+    public class StateBase
     {
-        Debug.Log("OnStateEnter");
-    } 
-    public virtual void OnStateStay (object o = null)
-    {
-        Debug.Log("OnStateStay");
-    }
-    public virtual void OnStateExit (object o = null)
-    {
-        Debug.Log("OnStateExit");
+        public virtual void OnStateEnter(object o = null)
+        {
+            Debug.Log("OnStateEnter");
+        } 
+        public virtual void OnStateStay (object o = null)
+        {
+            Debug.Log("OnStateStay");
+        }
+        public virtual void OnStateExit (object o = null)
+        {
+            Debug.Log("OnStateExit");
+        }
     }
 }
