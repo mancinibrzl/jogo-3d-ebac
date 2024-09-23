4f27fc17d1f4ac95037070a6f5eaefad260ffa57
mancinibrzl <mariomancinineto2017@gmail.com>
2024-09-23 17:02:34 -0300

    Update GunBase.cs

--- Jogo 3D Unity/Assets/Scripts/Gun/GunBase.cs
+++ Jogo 3D Unity/Assets/Scripts/Gun/GunBase.cs
@@ -13,20 +13,7 @@ public class GunBase : MonoBehaviour
 
     public KeyCode keyCode = KeyCode.Z;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(keyCode))
-        {
-            _currentCoroutine = StartCoroutine(StartShoot());
-        }
-        else if (Input.GetKeyUp(keyCode))
-        {
-            if (_currentCoroutine != null) 
-                StopCoroutine(_currentCoroutine);
-        }
-    }
-
-    IEnumerator StartShoot()
+    IEnumerator ShootCoroutine()
     {
         while (true)
         {
@@ -41,5 +28,17 @@ public class GunBase : MonoBehaviour
         var projectile = Instantiate(prefabProjectile);
         projectile.transform.position = positionToShoot.position;
         projectile.transform.rotation = positionToShoot.rotation;
+    }
+
+    public void StartShoot()
+    {
+        StopShoot();
+        _currentCoroutine = StartCoroutine(ShootCoroutine());
+    }
+
+    public void StopShoot()
+    {
+        if (_currentCoroutine != null)
+            StopCoroutine(_currentCoroutine);
     }
 }
