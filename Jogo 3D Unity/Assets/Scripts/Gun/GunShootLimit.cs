61f8d272a6952999a20741a8edd376076808bce1
mancinibrzl <mariomancinineto2017@gmail.com>
2024-09-25 14:08:47 -0300

    Update GunShootLimit.cs

--- Jogo 3D Unity/Assets/Scripts/Gun/GunShootLimit.cs
+++ Jogo 3D Unity/Assets/Scripts/Gun/GunShootLimit.cs
@@ -1,23 +1,25 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShootLimit : GunBase
 {
+    public List<UIGunUpdater> uIGunUpdaters;
+
     public float maxShoot = 5f;
     public float timeToRecharge = 1f;
 
     private float _currentShoots;
     private bool _recharging = false;
 
+    private void Awake()
+    {
+        GetAllUIs();
+    }
+
     protected override IEnumerator ShootCoroutine()
     {
-        /*while (true)
-        {
-            Shoot();
-            yield return new WaitForSeconds(timeBetweenShoot);
-        }*/
-
         if(_recharging) yield break;
 
         while (true)
@@ -27,6 +29,7 @@ public class GunShootLimit : GunBase
                 Shoot();
                 _currentShoots++;
                 CheckRecharge();
+                UpdateUI();
                 yield return new WaitForSeconds(timeBetweenShoot);
             }
         }
@@ -53,10 +56,20 @@ public class GunShootLimit : GunBase
         while (time < timeToRecharge)
         {
             time += Time.deltaTime;
-            Debug.Log("Rechargin: " + time);
+            uIGunUpdaters.ForEach(i => i.UpdateValue(time/timeToRecharge));
             yield return new WaitForEndOfFrame();
         }
         _currentShoots = 0;
         _recharging = false;
+    }
+
+    private void UpdateUI()
+    {
+        uIGunUpdaters.ForEach(i => i.UpdateValue(maxShoot, _currentShoots));
+    }
+
+    private void GetAllUIs()
+    {
+        uIGunUpdaters = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
     }
 }
