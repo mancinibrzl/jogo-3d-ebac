29b72d9129ca22bffed0e7e5e0dab760b9bb43a3
mancinibrzl <mariomancinineto2017@gmail.com>
2024-09-27 12:25:46 -0300

    Update PlayerAbilityShoot.cs

--- Jogo 3D Unity/Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Jogo 3D Unity/Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -5,26 +5,49 @@ using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
-    public GunBase gunBase;
+    [SerializeField] private GunBase[] _gunPrefabs;
     public Transform gunPosition;
-
+    
     private GunBase _currentGun;
+
+    private GunBase[] _gunInstances;
 
     protected override void Init()
     {
         base.Init();
 
-        CreateGun();
+        CreateGuns();
 
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
+
+        inputs.Gameplay.ChangeGun1.performed += ctx => ChangeGun(0);
+        inputs.Gameplay.ChangeGun2.performed += ctx => ChangeGun(1);
     }
 
-    private void CreateGun()
+    private void CreateGuns()
     {
-        _currentGun = Instantiate(gunBase, gunPosition);
+        _gunInstances = new GunBase[_gunPrefabs.Length];
 
-        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        for (int i = 0; i < _gunPrefabs.Length; ++i)
+        {
+            _gunInstances[i] = Instantiate(_gunPrefabs[i], gunPosition);
+            _gunInstances[i].transform.localPosition = _gunInstances[i].transform.localEulerAngles = Vector3.zero;
+            _gunInstances[i].gameObject.SetActive(false);
+        }
+
+        _currentGun = _gunInstances[0];
+        _currentGun.gameObject.SetActive(true);
+    }
+
+    private void ChangeGun(int index)
+    {
+        if(_currentGun == _gunInstances[index])
+           return;
+
+        _currentGun.gameObject.SetActive(false);
+        _currentGun = _gunInstances[index];
+        _currentGun.gameObject.SetActive(true);
     }
 
     private void StartShoot()
