c192d62e7f0c373991b89c4c030344929f263d87
mancinibrzl <mariomancinineto2017@gmail.com>
2024-10-07 14:39:56 -0300

    Update EnemyBase.cs

--- Jogo 3D Unity/Assets/Scripts/Enemies/EnemyBase.cs
+++ Jogo 3D Unity/Assets/Scripts/Enemies/EnemyBase.cs
@@ -59,6 +59,8 @@ namespace Enemy
             if (flashColor != null) flashColor.Flash();
             if (particleSystem != null) particleSystem.Emit(15);
 
+            transform.position -= transform.forward;
+
             _currentLife -= f;
 
             if (_currentLife <= 0)
@@ -85,6 +87,10 @@ namespace Enemy
             Debug.Log("Damage");
             OnDamage(damage);
         }
-
+        public void Damage(float damage, Vector3 dir)
+        {
+            OnDamage(damage);
+            transform.DOMove(transform.position - dir, .1f);
+        }
     }
 }
